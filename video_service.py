11f1443019d80ff4852493d6e934090cc61d3440
A001ZA <danusorn2579@gmail.com>
2026-05-30 20:05:42 +0700

    Use environment variable for server port

--- video_service.py
+++ video_service.py
@@ -252,4 +252,5 @@ def assemble():
 if __name__ == '__main__':
     # ดาวน์โหลดฟอนต์ตอน startup เลย (ไม่ต้องรอ request แรก)
     ensure_font()
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
